a7d22f45e047bbeb1d34625cfeacaf78368b0310
Konark-2ez <konarkkumar456@gmail.com>
2023-05-19 22:49:54 +0530

    added palindrome of a number

---
+++ Math/numberpalindrome.js
@@ -0,0 +1,41 @@
+//  Given a number check if it is a palindrome.
+function checkPalindrome(n){
+    let rev = 0
+    while(n>0){
+        let rem = n%10;
+        rev = (rev*10)+rem
+        n = n/10
+    }
+    return rev
+}
+function runProgram(input){
+    var input = input.trim().split("\n")
+    var tc = +input[0]
+    var N = +input[1]
+    if(checkPalindrome(N)===n){
+        console.log("Yes it is a palindrome")
+    }else{
+        console.log("No")
+    }
+}
+if (process.env.USER === "") {
+    runProgram(`1
+    252`);
+} else {
+        process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  let read = "";
+  process.stdin.on("data", function (input) {
+        read += input;
+  });
+  process.stdin.on("end", function () {
+        read = read.replace(/\n$/, "");
+    read = read.replace(/\n$/, "");
+    runProgram(read);
+  });
+  process.on("SIGINT", function () {
+        read = read.replace(/\n$/, "");
+    runProgram(read);
+    process.exit(0);
+  });
+}
--- README.md
+++ README.md
@@ -6,3 +6,8 @@
  -&#128512; 3) FizzBuzz
  -&#128512; 4) NumberOfGoodPairs -LeetCode(Easy)
  -&#128512; 5) ReverseInteger -LeetCode(Medium)
+
+ #  &#129300; Array Questions
+
+ Arrays in javascript can hold any data type  as its element. Arrays are collection of similar or non similar elements.
+ 
